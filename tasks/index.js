fe8715201d74cf60ea3d19e7e6a9f58cf28cb2b1
Richard King <richrdkng@gmail.com>
2015-12-27 20:05:51 +0100

    Add version tag task

--- tasks/index.js
+++ tasks/index.js
@@ -1,32 +1,45 @@
 var include = require('rekuire'),
 
+    gulp    = require('gulp'),
+    bump    = require('gulp-bump'),
+    tag     = require('gulp-tag-version'),
+
     project = include('project'),
+
     path    = project.path,
     root    = path.root,
 
-    gulp    = require('gulp'),
-    bump    = require('gulp-bump');
+    file    = {
+        package_json : root + '/package.json'
+    };
 
 // bump tasks
 gulp.task('bump', ['bump-minor']);
 
     gulp.task('bump-major', function() {
         return gulp
-            .src(root + './package.json')
+            .src(file.package_json)
             .pipe(bump({type : 'major'}))
-            .pipe(gulp.dest(root + '/'));
+            .pipe(gulp.dest(root));
     });
 
     gulp.task('bump-minor', function() {
         return gulp
-            .src(root + './package.json')
+            .src(file.package_json)
             .pipe(bump({type : 'minor'}))
-            .pipe(gulp.dest(root + '/'));
+            .pipe(gulp.dest(root));
     });
 
     gulp.task('bump-patch', function() {
         return gulp
-            .src(root + './package.json')
+            .src(file.package_json)
             .pipe(bump({type : 'patch'}))
-            .pipe(gulp.dest(root + '/'));
+            .pipe(gulp.dest(root));
     });
+
+// tag task
+gulp.task('tag', function() {
+    return gulp
+        .src(file.package_json)
+        .pipe(tag());
+});
